d31e6e5ed37cd9019d7a17b54cd2690039dc9a4d
Krishen Mohan <krishenm@gmail.com>
2020-11-08 15:17:57 +0800

    add human

---
+++ human.py
@@ -0,0 +1,24 @@
+from board import Board
+from player import Player
+
+class Human(Player):
+    """docstring for Human"""
+    def __init__(self, arg):
+        super(Human, self).__init__()
+
+    def get_best_move(self, board)
+        board.print()
+
+        move = self.prompt();
+
+        while (!board.is_move_valid())
+            print("Invalid move. Try harder sub-organism.")
+            move = self.prompt()
+
+        print("\nYou amuse us.\n")
+
+        board.execute(move)
+        return
+        
+    def prompt(self)
+        return int(input("\nEnter move index human...: \n"))
