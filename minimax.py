3310d0f87250e3460a8de74d78b25a8d9b53ff49
Krishen Mohan <krishenm@gmail.com>
2020-11-11 21:59:52 +0800

    it's alive

--- minimax.py
+++ minimax.py
@@ -15,8 +15,12 @@ class Minimax(Player):
         return self.filter(move_value_pairs)
 
     def filter(self, move_value_pairs):
+        factor = 1
+        if self.turn == 2:
+            factor = -1
+
         move, value = max(move_value_pairs,
-                          key=lambda pair: pair[1] * -1 ** (self.turn - 1))
+                          key=lambda pair: pair[1] * factor)
 
         return move
 
@@ -42,18 +46,21 @@ class Minimax(Player):
         if (new_board.is_game_over()):
             new_board.print()
             print("Simulation over, move score: %f" % (new_board.get_game_result() / new_board.get_depth()))
-            return  new_board.get_game_result() / new_board.get_depth()
+            return new_board.get_game_result() / new_board.get_depth()
 
         value = self.calculate_position_value(new_board)
-        self.cache.add(board, value/new_board.get_depth())
+        self.cache.add(board, value / new_board.get_depth())
 
         return value
 
     def calculate_position_value(self, board):
         moves = board.get_valid_moves()
 
-        move_values = [-1 ** (board.get_depth() + self.turn - 1) *
-                       self.get_move_value(move, board)
+        factor = 1
+        if (board.get_depth() + self.turn) % 2 == 0:
+            factor = -1
+
+        move_values = [factor * self.get_move_value(move, board)
                        for move in moves]
 
         return max(move_values)
